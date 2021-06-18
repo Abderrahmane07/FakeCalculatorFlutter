6ff9b36877e04990958f2ab4da09b63df8e42087
Abderrahmane07 <75677650+Abderrahmane07@users.noreply.github.com>
2021-06-18 04:25:04 +0200

    Create main.dart
    Two Containers placed in a clomun

---
+++ main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        FocusScopeNode currentFocus = FocusScope.of(context);
+
+        if(!currentFocus.hasPrimaryFocus) {
+          currentFocus.unfocus();
+        }
+      },
+
+      child: MaterialApp(
+        title: 'FriendlyChat',
+        home: Scaffold(
+          body: Column(
+            children: <Widget>[
+              Container(
+                child: TextField(
+                  decoration: InputDecoration.collapsed(hintText: 'jwana'),
+                ),
+                padding: EdgeInsets.all(100),
+                margin: EdgeInsets.all(50),
+                color: Colors.blue[50],
+              ),
+              Container(
+                child: TextField(
+                  decoration: InputDecoration.collapsed(hintText: 'jwana'),
+                ),
+                padding: EdgeInsets.all(100),
+                margin: EdgeInsets.all(50),
+                color: Colors.blue[200],
+              ),
+            ],
+          )
+          ),
+        ),
+    );
+  }
+}
