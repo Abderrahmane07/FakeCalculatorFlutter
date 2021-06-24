4e7fe7f553ccb3a835e8b495d6affad4f0b18f05
Abderrahmane07 <75677650+Abderrahmane07@users.noreply.github.com>
2021-06-24 16:47:06 +0200

    Add the import

--- main.dart
+++ main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:ninja/home.dart';
 import 'package:ninja/previous.dart';
+import 'package:ninja/coco.dart';
 
 void main() => runApp(MaterialApp(
   initialRoute: '/',
   routes: {
     '/': (context) => Home(),
     '/previous': (context) => Previous(),
+    '/coco': (context) => Coco(),
   },
 ));
 
