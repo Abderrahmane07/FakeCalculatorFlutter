e4c4d072ced1a1681dff551ec9617eb09274a522
Abderrahmane07 <75677650+Abderrahmane07@users.noreply.github.com>
2021-06-26 13:42:00 +0200

    Almost final

--- main.dart
+++ main.dart
@@ -2,13 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:ninja/home.dart';
 import 'package:ninja/previous.dart';
 import 'package:ninja/coco.dart';
+import 'package:ninja/newcontact.dart';
 
 void main() => runApp(MaterialApp(
+  //debugShowCheckedModeBanner: false,
   initialRoute: '/',
   routes: {
     '/': (context) => Home(),
     '/previous': (context) => Previous(),
     '/coco': (context) => Coco(),
+    '/newcontact': (context) => Newcontact(),
   },
 ));
 
