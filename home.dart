2a21c15793d29133a4ababb292fe4a5839dec0fc
Abderrahmane07 <75677650+Abderrahmane07@users.noreply.github.com>
2021-06-26 14:20:46 +0200

    Adding InkWell effect

--- home.dart
+++ home.dart
@@ -31,37 +31,41 @@ Map toto = {};
 TextEditingController _nameController = TextEditingController();
 Widget myWidget(BuildContext context) {
 
-  toto = ModalRoute.of(context)?.settings.arguments as Map;
+  toto = ModalRoute.of(context).settings.arguments as Map;
 
   fctBox(Color couleur, String symbol){
     return Expanded(
       flex: 25,
-      child: InkWell(
-        onTap: (){
-          print("Container clicked");
-         _nameController.text += symbol; 
-         //toto[0]='';
-         //print(toto.length);
-        },
-        child: Container(
-          height: 92,
-          child: Align(
-            alignment: Alignment.center,
-            child: Text(
-              symbol,
-              style: TextStyle(
-                              fontSize: 42,
-                              color: couleur,
-                              ),
+      child: Container(
+        child: Material(
+          child: InkWell(
+            onTap: (){
+              print("Container clicked");
+             _nameController.text += symbol; 
+            },
+            child: Container(
+              height: 92,
+              child: Align(
+                alignment: Alignment.center,
+                child: Text(
+                  symbol,
+                  style: TextStyle(
+                                  fontSize: 42,
+                                  color: couleur,
+                                  ),
+                ),
+              ),
+              
+              decoration: BoxDecoration(
+                //color: Colors.white,
+                border: Border.all(color: HexColor("#f0ebeb")) 
+                ),
             ),
           ),
-          
-          decoration: BoxDecoration(
-            color: Colors.white,
-            border: Border.all(color: HexColor("#f0ebeb")) 
-            ),
+          color: Colors.transparent,
         ),
-      )
+        color: Colors.white,
+      ),
     );
   }
   
@@ -105,56 +109,69 @@ Widget myWidget(BuildContext context) {
                 children: <Widget>[
                   Expanded(
                     flex: 25,
-                    child: InkWell(
-                      onTap: (){
-                        print("Container clicked");
-                      _nameController.text = ''; 
-                      //print(toto[0]);
-                      },
-                      child: Container(
-                        height: 92,
-                        child: Align(
-                          alignment: Alignment.center,
-                          child: Text(
-                            'C',
-                            style: TextStyle(
-                              fontSize: 35,
-                              color: Colors.red,
+                    child: Container(
+                      child: Material(
+                        child: InkWell(
+                          onTap: (){
+                            print("Container clicked");
+                            _nameController.text = ''; 
+                          },
+                          child: Container(
+                            height: 92,
+                            child: Align(
+                              alignment: Alignment.center,
+                              child: Text(
+                                'C',
+                                style: TextStyle(
+                                  fontSize: 35,
+                                  color: Colors.red,
+                                  ),
+                              ),
+                            ),
+                            
+                            decoration: BoxDecoration(
+                              //color: Colors.white,
+                              border: Border.all(color: HexColor("#f0ebeb")) 
                               ),
                           ),
                         ),
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          border: Border.all(color: HexColor("#f0ebeb")) 
-                          ),
+                        color: Colors.transparent,
                       ),
-                    )
+                      color: Colors.white,
+                    ),
                   ),
                   fctBox(Colors.blue, '/'),
                   fctBox(Colors.blue, 'x'),
                   Expanded(
                     flex: 25,
-                    child: InkWell(
-                      onTap: (){
-                        print("Container clicked");
-                      _nameController.text = _nameController.text.substring(0, _nameController.text.length-1); 
-                      },
-                      child: Container(
-                        height: 92,
-                        child: Align(
-                          alignment: Alignment.center,
-                          child: Icon(
-                            Icons.backspace_outlined,
-                            color: Colors.blue,
-                            size: 35,
+                    child: Container(
+                      child: Material(
+                        child: InkWell(
+                          onTap: (){
+                            print("Container clicked");
+                            _nameController.text = _nameController.text.substring(0, _nameController.text.length-1); 
+                          },
+                          child: Container(
+                            height: 92,
+                            child: Align(
+                              alignment: Alignment.center,
+                              child: Icon(
+                                Icons.backspace_outlined,
+                                color: Colors.blue,
+                                size: 35,
+                              ),
+                            ),
+                            
+                            decoration: BoxDecoration(
+                              //color: Colors.white,
+                              border: Border.all(color: HexColor("#f0ebeb")) 
+                              ),
                           ),
                         ),
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          border: Border.all(color: HexColor("#f0ebeb")) 
-                          ),
+                        color: Colors.transparent,
                       ),
-                    )
+                      color: Colors.white,
+                    ),
                   ),
               ],
               ),
@@ -179,33 +196,40 @@ Widget myWidget(BuildContext context) {
                   fctBox(HexColor("#525252"), '4'),
                   Expanded(
                     flex: 25,
-                    child: InkWell(
-                      onTap: (){
-                        print("Container clicked");
-                        _nameController.text += "5"; 
-                      },
-                      onLongPress: (){
-                        Navigator.pushNamed(context, '/previous');
-                        print("Long pressed on number 5, lets take you to your contacts");
-                      },
-                      child: Container(
-                        height: 92,
-                        child: Align(
-                          alignment: Alignment.center,
-                          child: Text(
-                            '5',
-                            style: TextStyle(
-                              color: HexColor("#525252"),
-                              fontSize: 42
+                    child: Container(
+                      child: Material(
+                        child: InkWell(
+                          onTap: (){
+                            print("Container clicked");
+                            _nameController.text += "5"; 
+                          },
+                          onLongPress: (){
+                            Navigator.pushNamed(context, '/previous');
+                            print("Long pressed on number 5, lets take you to your contacts");
+                          },
+                          child: Container(
+                            height: 92,
+                            child: Align(
+                              alignment: Alignment.center,
+                              child: Text(
+                                '5',
+                                style: TextStyle(
+                                  color: HexColor("#525252"),
+                                  fontSize: 42
+                                  ),
+                              ),
+                            ),
+                            
+                            decoration: BoxDecoration(
+                              //color: Colors.white,
+                              border: Border.all(color: HexColor("#f0ebeb")) 
                               ),
                           ),
                         ),
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          border: Border.all(color: HexColor("#f0ebeb")) 
-                          ),
+                        color: Colors.transparent,
                       ),
-                    )
+                      color: Colors.white,
+                    ),
                   ),
                   fctBox(HexColor("#525252"), '6'),
                   fctBox(Colors.blue, '+'),
@@ -231,60 +255,74 @@ Widget myWidget(BuildContext context) {
                 children: <Widget>[
                   Expanded(
                     flex: 50,
-                    child: InkWell(
-                      onTap: (){
-                        print("Big container clicked");
-                        _nameController.text += "0"; 
-                      },
-                      child: Container(
-                        height: 92,
-                        child: Align(
-                          alignment: Alignment.center,
-                          child: Text(
-                            '0',
-                            style: TextStyle(
-                              color: HexColor("#525252"),
-                              fontSize: 42
+                    child: Container(
+                      child: Material(
+                        child: InkWell(
+                          onTap: (){
+                            print("Big container clicked");
+                            _nameController.text += "0"; 
+                          },
+                          child: Container(
+                            height: 92,
+                            child: Align(
+                              alignment: Alignment.center,
+                              child: Text(
+                                '0',
+                                style: TextStyle(
+                                  color: HexColor("#525252"),
+                                  fontSize: 42
+                                  ),
+                              ),
+                            ),
+                            
+                            decoration: BoxDecoration(
+                              //color: Colors.white,
+                              border: Border.all(color: HexColor("#f0ebeb")) 
                               ),
                           ),
                         ),
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          border: Border.all(color: HexColor("#f0ebeb")) 
-                          ),
+                        color: Colors.transparent,
                       ),
-                    )
+                      color: Colors.white,
+                    ),
                   ),
                   fctBox(HexColor("#525252"), '.'),
                   Expanded(
                     flex: 25,
-                    child: InkWell(
-                      onTap: (){
-                        print("Container clicked");
-                      _nameController.text = toto['phone']; 
-                      },
-                      onLongPress: (){
-                        Navigator.pushNamed(context, '/coco');
-                        print("Long pressed on =, lets take you to your contacts");
-                      },
-                      child: Container(
-                        height: 92,
-                        child: Align(
-                          alignment: Alignment.center,
-                          child: Text(
-                            '=',
-                            style: TextStyle(
-                                            fontSize: 50,
-                                            color: Colors.white,
-                                            ),
+                    child: Container(
+                      child: Material(
+                        child: InkWell(
+                          onTap: (){
+                            print("Container clicked");
+                            _nameController.text = toto['phone']; 
+                          },
+                          onLongPress: (){
+                            Navigator.pushNamed(context, '/coco');
+                            print("Long pressed on =, lets take you to your contacts");
+                          },
+                          child: Container(
+                            height: 92,
+                            child: Align(
+                              alignment: Alignment.center,
+                              child: Text(
+                                '=',
+                                style: TextStyle(
+                                  fontSize: 50,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                            
+                            decoration: BoxDecoration(
+                              //color: Colors.white,
+                              border: Border.all(color: HexColor("#f0ebeb")) 
+                              ),
                           ),
                         ),
-                        decoration: BoxDecoration(
-                          color: Colors.blue,
-                          border: Border.all(color: HexColor("#f0ebeb")) 
-                          ),
+                        color: Colors.transparent,
                       ),
-                    )
+                      color: Colors.blue,
+                    ),
                   ),
               ],
               ),
